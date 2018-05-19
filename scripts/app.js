05e6a74cd97d24f39d16b7d260133559e7772f9e
brianbier <briansbier@gmail.com>
2018-05-19 16:22:21 -0400

    copy updates

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,7 @@ var app = new Vue({
     projects: {
       'coast-guard-foundation': {
         image: '/images/coastguardfoundation.jpg',
-        description: 'The Coast Guard Foundation is a nonprofit that provides financial and material support to students at the Coast Guard Academy, members of the Coast Guard, Coast Guard Auxiliary, and their families. As developer, I focused on building a responsive website that puts the clients photos, philosophy and simplistic navigation front and centered. It was built using Nuxt.js and Tachyons css utility classes. Nuxt.js provideds all configurations needed to make developing server render applications more enjoyable using Vue.js. Tachyon is a new framework that focuses on custom design without writting css. It also makes it easier to follow by other developers and not having to figure out what each class is doing.',
+        description: 'The Coast Guard Foundation is a nonprofit that provides financial and material support to students at the Coast Guard Academy, members of the Coast Guard, Coast Guard Auxiliary. I mainly focused on building a responsive website with a  simplistic look and feel. It was built using Nuxt.js and Tachyons CSS utility classes. Nuxt.js provides all configurations needed to make developing server render applications more enjoyable while using Vue.js. Tachyon is a new framework that focuses on custom design without writing CSS.',
         title: 'Coast Guard Foundation:',
         type: 'Web Development',
         subHeading: 'Supporting those who protect our nation',
@@ -17,7 +17,7 @@ var app = new Vue({
       },
       'peconic-land-trust': {
         image: '/images/peconic.png',
-        description: 'Peconic Land Trust works to protect over 12,000 acres of natural lands on Long Island. I focused on building a beautiful responsive website that was more appealing to their active users groups. I built the website from start to finish and developed Vue.js components to handle pages of the website, such as the calendar and places to visit. I built an API endpoint to manage their events making it easier for users to flow throught the calendar page.',
+        description: 'Peconic Land Trust works to protect over 12,000 acres of natural lands on Long Island. I focused on building a beautiful responsive website that was more appealing to their active user\'s. I built the website using Twig as a templating language and Vue.js components. Examples of the Vue components can be found on the Places to visit page and the Calendar Page.',
         title: 'Peconic Land Trust:',
         type: 'Web Development',
         subHeading: 'Conserving Long Island',
@@ -27,7 +27,7 @@ var app = new Vue({
       },
       'eyetoeye': {
         image: '/images/eyetoeye.png',
-        description: 'Eye to eye helps transition students from a place of discomfort to a newfound sense of assurance and belonging. As a developer, I decided to create alot of resuable Vue.js components and Tailwind css Utility classes. It helped with the rapid development of this website. I implemented Vue.js throughout the website for example in the mentoring page. I built a flickity slider component that handles all sliders throughout the site and created a google maps component that shows all of the clients chapters thorught the United States.',
+        description: 'Eye to eye helps transition students from a place of discomfort to a newfound sense of assurance and belonging. As a developer, I decided to create a lot of reusable Vue.js components while utilizing Tailwind CSS Utility classes. It helped with the rapid development of this website. I implemented Vue.js throughout the website for example on the mentoring page. I built a flickity component that handles all sliders throughout the site and created a google maps component that shows all of the client\'s chapters throughout the United States.',
         title: 'Eye To Eye:',
         type: 'Web Development',
         subHeading: 'It takes 1 to help 1',
@@ -37,7 +37,7 @@ var app = new Vue({
       },
       'cgc-therapy': {
         image: '/images/cgc-therapy.jpg',
-        description: 'Cgc therapy was established in 2010 providing the highest quality of psychotherapy services to individuals, couples, and families. As a Freelancer I was tasked to design and develope a new website that made it easy for the public to feel welcoming in the most simplistic way possible.',
+        description: 'Cgc therapy was established in 2010 providing the highest quality of psychotherapy services to individuals, couples, and families. As a Freelancer, I was tasked to design and develop a new website that made it easy for the public to feel welcome in the most simplistic way possible.',
         title: 'cgc therapy:',
         type: 'Web Development',
         subHeading: 'A Common Ground',
@@ -47,7 +47,7 @@ var app = new Vue({
       },
       'sasha-ostojic': {
         image: '/images/sashaostojic.png',
-        description: 'By presenting ideas the right way, as digestible, data-driven insights, ideas can spark conversations and collaborations to deliver real world solutions and create a positive impact in a variety of topics. As a freelancer I volunteer to built a fully functional website using Ruby on Rails which helped with the understanding of creating a CRUD application.',
+        description:'By presenting ideas the right way, as digestible, data-driven insights, ideas can spark conversations and collaborations to deliver real-world solutions and create a positive impact on a variety of topics. As a freelancer, I volunteer to build a fully functional website using Ruby on Rails which helped with the understanding of creating a CRUD application and Utilizing gems.',
         title: 'Sasha Ostojic:',
         type: 'Web Development',
         subHeading: 'Digestible Data-Driven Insights',
